3572f42098451ad892ec984c5eb2fea802383709
Alexander Anthon <alex.r.anthon@gmail.com>
2021-08-23 13:13:15 -0600

    Added code to read network classes. Added network classes.

--- fileio/readnetworkclass.go
+++ fileio/readnetworkclass.go
@@ -2,30 +2,73 @@ package fileio
 
 import (
 	"archive/tar"
+	"compress/gzip"
 	"errors"
 	"io"
+	"io/fs"
 	"os"
 	"path/filepath"
+	"regexp"
+	"sort"
+	"strconv"
 
 	"github.com/GaudiestTooth17/irn-sim/network"
 )
 
+var idMatcher *regexp.Regexp = regexp.MustCompile(`\d+`)
+
+// pathToClass must point to a .tar.gz file
 func ReadClass(pathToClass string) []*network.AdjacencyList {
-	reader, err := os.Open(pathToClass)
-	if err != nil {
-		panic(err)
+	// extract the compressed networks into /tmp (which is hopefully stored in RAM)
+	className := filepath.Base(pathToClass)
+	//strip extension
+	className = className[:len(className)-7]
+	extractionDest := filepath.Join("/tmp", className)
+	if _, err := os.Stat(extractionDest); os.IsNotExist(err) {
+		os.Mkdir(extractionDest, fs.ModePerm)
+		ungzipAndUntar(extractionDest, pathToClass)
 	}
-	defer reader.Close()
-	tarReader := tar.NewReader(reader)
+
+	// collect all instances of networks=
+	classInstances := make([]string, 0)
+	filepath.WalkDir(extractionDest, func(path string, d fs.DirEntry, err error) error {
+		filename := filepath.Base(path)
+		isClassInstance, _ := regexp.Match(`instance-\d+.txt`, []byte(filename))
+		if fileInfo, _ := os.Stat(path); !fileInfo.IsDir() && isClassInstance {
+			classInstances = append(classInstances, path)
+		}
+		return nil
+	})
+	// sort according to the ID's at the end of the file name
+	sort.Slice(classInstances, func(i, j int) bool {
+		nameI := filepath.Base(classInstances[i])
+		nameJ := filepath.Base(classInstances[j])
+		idI := atoiOrPanic(idMatcher.FindString(nameI))
+		idJ := atoiOrPanic(idMatcher.FindString(nameJ))
+		return idI < idJ
+	})
+
+	// load the networks into memory
+	nets := make([]*network.AdjacencyList, len(classInstances))
+	for i, pathToInstance := range classInstances {
+		nets[i] = ReadFile(pathToInstance)
+	}
+	return nets
 }
 
-func untar(tarball, target string) {
-	reader, err := os.Open(tarball)
+func ungzipAndUntar(target, gzippedTarball string) {
+	file, err := os.Open(gzippedTarball)
 	if err != nil {
 		panic(err)
 	}
-	defer reader.Close()
-	tarReader := tar.NewReader(reader)
+	defer file.Close()
+	gzipReader, err := gzip.NewReader(file)
+	if err != nil {
+		panic(err)
+	}
+	defer gzipReader.Close()
+
+	tarReader := tar.NewReader(gzipReader)
 
 	for {
 		header, err := tarReader.Next()
@@ -35,8 +78,11 @@ func untar(tarball, target string) {
 			panic(err)
 		}
 
-		path := filepath.Join(target, header.Name)
+		path := filepath.Join(target, filepath.Base(header.Name))
 		info := header.FileInfo()
+		if info.IsDir() {
+			continue
+		}
 
 		file, err := os.OpenFile(path, os.O_CREATE|os.O_TRUNC|os.O_WRONLY, info.Mode())
 		if err != nil {
@@ -44,5 +90,16 @@ func untar(tarball, target string) {
 		}
 		defer file.Close()
 		_, err = io.Copy(file, tarReader)
+		if err != nil {
+			panic(err)
+		}
 	}
+}
+
+func atoiOrPanic(str string) int {
+	integer, err := strconv.Atoi(str)
+	if err != nil {
+		panic(err)
+	}
+	return integer
 }
--- main.go
+++ main.go
@@ -2,11 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
 	"os"
 
 	fio "github.com/GaudiestTooth17/irn-sim/fileio"
-	"github.com/GaudiestTooth17/irn-sim/sim"
 )
 
 func main() {
@@ -16,15 +14,18 @@ func main() {
 	}
 	networkPath := os.Args[1]
 
-	// set up the parameters
-	rng := rand.New(rand.NewSource(0))
-	net := fio.ReadFile(networkPath)
-	disease := sim.Disease{DaysInfectious: 4, TransProb: .2}
-	behavior := sim.NewSimplePressureBehavior(net, rng, 2, .25)
-	// behavior := sim.StaticBehavior{}
-	sir0 := sim.MakeSir0(net.N(), 1, rng)
-	// run a simulation
-	// result := sim.Simulate(net.M(), sir0, disease, behavior, 300, rng)
-	// fmt.Println(sim.GetSurvivalPercentage(result))
-	fmt.Println(runSimBatch(net.M(), sir0, disease, behavior, 100, rng, 500))
+	nets := fio.ReadClass(networkPath)
+	fmt.Printf("Read %d networks.\n", len(nets))
+
+	// // set up the parameters
+	// rng := rand.New(rand.NewSource(0))
+	// net := fio.ReadFile(networkPath)
+	// disease := sim.Disease{DaysInfectious: 4, TransProb: .2}
+	// behavior := sim.NewSimplePressureBehavior(net, rng, 2, .25)
+	// // behavior := sim.StaticBehavior{}
+	// sir0 := sim.MakeSir0(net.N(), 1, rng)
+	// // run a simulation
+	// // result := sim.Simulate(net.M(), sir0, disease, behavior, 300, rng)
+	// // fmt.Println(sim.GetSurvivalPercentage(result))
+	// fmt.Println(runSimBatch(net.M(), sir0, disease, behavior, 100, rng, 500))
 }
